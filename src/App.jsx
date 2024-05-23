fd35ad8d6233dbca5a895bd7160c9203cf154b66
2haseeb1 <prodhanhome@gmail.com>
2024-05-23 17:59:42 +0600

    first initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h3 className="text-center text-5xl">Hello World</h3>
+      <div>
+        <button className="btn btn-primary">Ok</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
