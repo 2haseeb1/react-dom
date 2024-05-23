fe442395324c783b7ceb7d13dd48e4783247e760
2haseeb1 <prodhanhome@gmail.com>
2024-05-23 19:46:13 +0600

    second commit adding banner

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+
+
+const Banner = () => {
+  return (
+    <div>
+      <div className="hero h-[550px] bg-base-200">
+  <div className="hero-content flex-col lg:flex-row-reverse">
+    <img src="https://i.ibb.co/f1jPcy9/pngwing-1.png" className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Books to freshen up your bookshelf</h1>
+    
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Banner
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import Banner from "./Banner";
 const Home = () => {
   return (
-    <div>
+    <div className="w-[1170px] mx-auto">
       <Header></Header>
+      <Banner></Banner>
       <Outlet></Outlet>
     </div>
   );
